1da899526b6b5650f78dc615934ab3ad37ad27c9
zyl910 <zyl910@sina.com>
2017-02-23 17:05:10 +0800

    Fix zinfoassemblylist project .

--- system/zinfoassemblylist/MyInfo.cs
+++ system/zinfoassemblylist/MyInfo.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Reflection;
-using zyllibcs.system;
-using zyllibcs.text;
+using ZylLib.System;
+using ZylLib.Text;
 
 namespace zinfoassemblylist {
 	/// <summary>
