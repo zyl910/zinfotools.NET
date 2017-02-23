e76c59228b9f67331214a407fca7bc60d993c911
zyl910 <zyl910@sina.com>
2017-02-23 17:10:00 +0800

    Fix zinfoculture project

--- system/zinfoculture/MyInfo.cs
+++ system/zinfoculture/MyInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Globalization;
 using System.Reflection;
-using zyllibcs.text;
+using ZylLib.Text;
 
 namespace zinfoculture {
 	/// <summary>
--- system/zinfoculture/Program.cs
+++ system/zinfoculture/Program.cs
@@ -1,7 +1,7 @@
 using System;
 //using System.Collections.Generic;
 //using System.Text;
-using zyllibcs.text;
+using ZylLib.Text;
 
 namespace zinfoculture {
 	/// <summary>
